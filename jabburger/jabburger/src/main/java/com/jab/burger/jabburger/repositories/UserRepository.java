e059c2dee2ecec9fb50884c6f6cdaf5632d1479a
Zergexc <sebastian23155@outlook.es>
2024-10-24 12:59:13 -0500

    Eliminando carpeta jabburger duplicada

--- jabburger/jabburger/src/main/java/com/jab/burger/jabburger/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jab.burger.jabburger.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.jab.burger.jabburger.models.User;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-}
